4359fbf655424eaf8a568ac5fa74bf4f91617b42
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-11-01 20:07:16 -0300

    Com calculo da raiz

--- dtr.py
+++ dtr.py
@@ -1,6 +1,6 @@
 n = float(input('Digite um número: '))
 dobro = n*2
 triplo = n*3
-raiz = n**2
+raiz = n** (1/2)
 
-print(' O dobro é {} \n O triplo é {} \n A raiz quadrada é {}'.format(dobro,triplo,raiz))
+print(' O dobro é {} \n O triplo é {} \n A raiz quadrada é {:.2f}'.format(dobro,triplo,raiz))
